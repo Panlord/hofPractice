4b281c49e080efc0e071173d6fa7461f2fd7abf6
Panlord <adpan.dev@gmail.com>
2022-05-09 16:18:09 -0700

    implement startsWith

--- practice.js
+++ practice.js
@@ -50,7 +50,12 @@ var onlyOneFruit = function(fruits, targetFruit) {
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
-
+  var startingWith = _.filter(fruits, function(fruit) {
+    if (fruit[0] === letter) {
+      return true;
+    }
+  });
+  return startingWith;
 };
 
 // return a filtered array containing only cookie-type desserts.
